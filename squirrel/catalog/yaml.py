debc71d527b24c99a8c3be203f61efd3af996680
Alp Aribal <alparbal@gmail.com>
2022-02-13 19:10:38 +0100

    solve circular import

--- squirrel/catalog/yaml.py
+++ squirrel/catalog/yaml.py
@@ -1,10 +1,12 @@
+from __future__ import annotations
+
 from dataclasses import dataclass, field
 from typing import Dict, List
 
 import ruamel.yaml
 
 from squirrel import __version__ as sq_ver
-from squirrel.catalog import Catalog, CatalogKey, Source
+from squirrel import catalog
 
 __all__ = ["prep_yaml", "catalog2yamlcatalog", "yamlcatalog2catalog"]
 
@@ -14,7 +16,7 @@ def prep_yaml() -> ruamel.yaml.YAML:
     yaml = ruamel.yaml.YAML()
     yaml.register_class(YamlSource)
     yaml.register_class(YamlCatalog)
-    yaml.register_class(CatalogKey)
+    yaml.register_class(catalog.CatalogKey)
     return yaml
 
 
@@ -33,25 +35,22 @@ class YamlCatalog:
     sources: List[YamlSource] = field(default_factory=list)
 
 
-def catalog2yamlcatalog(catobj: Catalog) -> YamlCatalog:
+def catalog2yamlcatalog(catobj: catalog.Catalog) -> YamlCatalog:
     """Convert a Catalog into a serializable catalog DTO"""
-    sources = []
-    # flatten version graph
-    for k, s in catobj.sources.items():
-        for sv in s.versions.values():
-            sources.append(
-                YamlSource(
-                    identifier=k,
-                    driver_name=sv.driver_name,
-                    driver_kwargs=sv.driver_kwargs,
-                    version=sv.version,
-                    metadata=sv.metadata,
-                )
-            )
+    sources = [
+        YamlSource(
+            identifier=iden,
+            driver_name=source.driver_name,
+            driver_kwargs=source.driver_kwargs,
+            version=source.version,
+            metadata=source.metadata,
+        )
+        for iden, source in catobj
+    ]
     return YamlCatalog(sources=sources)
 
 
-def yamlcatalog2catalog(yamlcat: YamlCatalog) -> Catalog:
+def yamlcatalog2catalog(yamlcat: YamlCatalog) -> catalog.Catalog:
     """Convert a serializable catalog DTO into a Catalog"""
     yamlcat = YamlCatalog(**yamlcat.__dict__)  # recreate to set default values
 
@@ -59,11 +58,11 @@ def yamlcatalog2catalog(yamlcat: YamlCatalog) -> Catalog:
     ver_split = [int(x) for x in yamlcat.version.split(".")]
     assert ver_split[0] > 0 or (ver_split[0] == 0 and ver_split[1] >= 4)
 
-    cat = Catalog()
+    cat = catalog.Catalog()
     for s in yamlcat.sources:
         s_cp = YamlSource(**s.__dict__)  # recreate to set default values
         # flat list to version graph
-        cat[s_cp.identifier][s_cp.version] = Source(
+        cat[s_cp.identifier, s_cp.version] = catalog.Source(
             driver_name=s_cp.driver_name,
             driver_kwargs=dict(**s_cp.driver_kwargs),
             metadata=dict(**s_cp.metadata),
