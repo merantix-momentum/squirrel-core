deb22db756a05310304708e13a7ee08fcbeaae87
Alp Aribal <alparbal@gmail.com>
2022-05-18 11:42:48 +0200

    revise test to demonstrate bug

--- test/test_driver/test_sourcecombiner.py
+++ test/test_driver/test_sourcecombiner.py
@@ -1,15 +1,23 @@
+from tempfile import TemporaryDirectory
+
+import numpy as np
+import pandas as pd
+
 from squirrel.catalog import Catalog, CatalogKey
 from squirrel.catalog.source import Source
 from squirrel.constants import URL
 
 
-def test_combiner_in_catalog(test_path: URL) -> None:
+def test_combiner_in_catalog() -> None:
     """Test if sources can be combined into a source combiner"""
     c = Catalog()
+    tmpdir = TemporaryDirectory()
 
-    c["train"] = Source("file", driver_kwargs={"path": test_path + "train.dummy"})
-    c["val"] = Source("file", driver_kwargs={"path": test_path + "val.dummy"})
-    c["test"] = Source("file", driver_kwargs={"path": test_path + "test.dummy"})
+    for split in ("train", "val", "test"):
+        fname = f"{tmpdir.name}/{split}.csv"
+        data = pd.DataFrame(dict(split=[split]))  # one row, one col df, only value of "split" changes
+        data.to_csv(fname)
+        c[split] = Source("csv", driver_kwargs={"path": fname})
 
     c["combined"] = Source(
         "source_combiner",
@@ -24,9 +32,11 @@ def test_combiner_in_catalog(test_path: URL) -> None:
 
     d = c["combined"].get_driver()
     assert len(d.subsets) == 3
-    assert d.get_source("subset1").get_driver().path == test_path + "train.dummy"
-    assert d.get_source("subset2").get_driver().path == test_path + "val.dummy"
-    assert d.get_source("subset3").get_driver().path == test_path + "test.dummy"
+    assert d.get_source("subset1").get_driver().path == f"{tmpdir.name}/train.csv"
+    assert d.get_source("subset2").get_driver().path == f"{tmpdir.name}/val.csv"
+    assert d.get_source("subset3").get_driver().path == f"{tmpdir.name}/test.csv"
+    all_rows = d.get_iter().collect()
+    assert [r.split for r in all_rows] == ["train", "val", "test"]
 
 
 def test_copy_combiner(test_path: URL) -> None:
