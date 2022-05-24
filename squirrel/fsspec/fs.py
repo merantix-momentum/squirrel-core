73ed4ac908b6f01975d790984e4410b621a19b4b
Tiansu <tiansuyu2020@icloud.com>
2022-05-24 17:06:55 +0200

    change squirrel test using a tmp public bucket (#46)
    * change squirrel test using a tmp public bucket
    
    * allow fs module to use requester pay
    
    * route test to a true requester pay bucket
    
    * typo
    
    * change requester pay env var to bool
    
    * remove the env var
    
    * switch back to the right bucket

--- squirrel/fsspec/fs.py
+++ squirrel/fsspec/fs.py
@@ -15,6 +15,22 @@ def get_fs_from_url(url: URL, **storage_options) -> FILESYSTEM:
 
     If the protocol "gs" is detected, will return a custom squirrel gcs file system instance which is more robust. For
     details, see :py:class:`squirrel.fsspec.custom_gcsfs.CustomGCSFileSystem`).
+
+    Users must have one of these IAM right in the respective projects to be able to access a requester pay bucket:
+    (Copied from https://issuetracker.google.com/issues/156960628.)
+
+    Requesters who include a billing project in their request. The project used in the request must be in good standing,
+    and the user must have a role in the project that contains the serviceusage.services.use permission.
+    The Editor and Owner roles contain the required permission.
+
+    Requesters who don't include a billing project but have resourcemanager.projects.createBillingAssignment permission
+    for the project that contains the bucket. The Billing Project Manager role contains the required permission.
+    Access charges associated with these requests are billed to the project that contains the bucket.
+
+    If you have encountered `ValueError: Bucket is requester pays. Set `requester_pays=True` when creating the
+    GCSFileSystem.` I suggest you instead of passing `requester_pays=True` to `storage_options` in fsspec, simply
+    switch to the right project by `gcloud config set project PROJECT_ID` where you have the right role(s). The problem
+    should be resolved by itself.
     """
     protocol, _ = split_protocol(url)
     return fsspec.filesystem(protocol, **storage_options)
