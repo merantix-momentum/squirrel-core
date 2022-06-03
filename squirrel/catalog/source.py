3c0018cf84e65a70cf0b70432b9ca4b2a5df44be
Alp Aribal <alparbal@gmail.com>
2022-06-03 15:14:51 +0200

    minor refactor in source

--- squirrel/catalog/source.py
+++ squirrel/catalog/source.py
@@ -1,6 +1,6 @@
 import json
-from dataclasses import dataclass, field
-from typing import Dict, Optional
+from dataclasses import asdict, dataclass, field
+from typing import Dict
 
 __all__ = ["Source"]
 
@@ -8,9 +8,8 @@ __all__ = ["Source"]
 @dataclass(init=True)
 class Source:
     driver_name: str
-    driver_kwargs: Optional[Dict[str, str]] = field(default_factory=dict)
-    metadata: Optional[Dict[str, str]] = field(default_factory=dict)
+    driver_kwargs: Dict[str, str] = field(default_factory=dict)
+    metadata: Dict[str, str] = field(default_factory=dict)
 
     def __repr__(self) -> str:  # noqa D105
-        dct = {"driver_name": self.driver_name, "driver_kwargs": self.driver_kwargs, "metadata": self.metadata}
-        return json.dumps(dct, indent=2, default=str)
+        return json.dumps(asdict(self), indent=2, default=str)
