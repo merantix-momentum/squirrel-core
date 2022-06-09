87a10c5777d6cb002a7a983bc0f66b79114605c2
Alireza Sohofi <a.sohofi@gmail.com>
2022-06-09 13:01:17 +0200

    add sphinxcontrib-mermaid

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,7 @@ extensions = [
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.todo",
     "autoapi.extension",
+    "sphinxcontrib.mermaid",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
