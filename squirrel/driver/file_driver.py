a1e57ddf55bae6f66a26fc6e3790ecff8d20d5b7
Alexander Koenig <50868646+axkoenig@users.noreply.github.com>
2022-09-15 16:20:41 +0200

    Fix file driver storage options (#85)
    * Fix storage_options
    
    * Version bump
    
    * Unpack storage_options
    
    * Integrate PR comments
    
    * Bump version
    
    * Simplify kwargs creation
    
    Co-authored-by: Alp Arıbal <AlpAribal@users.noreply.github.com>
    
    * Improve docs
    
    Co-authored-by: Alp Arıbal <AlpAribal@users.noreply.github.com>

--- squirrel/driver/file_driver.py
+++ squirrel/driver/file_driver.py
@@ -1,20 +1,23 @@
-from typing import IO
+from typing import IO, Any, Dict, Optional
 
+import fsspec
 from squirrel.driver.driver import Driver
 
 
 class FileDriver(Driver):
     name = "file"
 
-    def __init__(self, path: str, **kwargs) -> None:
+    def __init__(self, path: str, storage_options: Optional[Dict[str, Any]] = None, **kwargs) -> None:
         """Initializes FileDriver.
 
         Args:
             path (str): Path to a file.
+            storage_options (Optional[Dict[str, Any]]): a dict with keyword arguments passed to file system initializer
             **kwargs: Keyword arguments passed to the super class initializer.
         """
         super().__init__(**kwargs)
         self.path = path
+        self.storage_options = storage_options if storage_options is not None else {}
 
     def open(self, mode: str = "r", create_if_not_exists: bool = False, **kwargs) -> IO:
         """Returns a handler for the file.
@@ -27,13 +30,12 @@ class FileDriver(Driver):
                 to "r".
             create_if_not_exists (bool): If True, the file will be created if it does not exist (along with the parent
                 directories). This is achieved by providing `auto_mkdir=create_if_not_exists` as a storage option to
-                the filesystem. Defaults to False.
+                the filesystem. No matter what you set in the FileDriver's `storage_options`, `create_if_not_exists`
+                will override the key `auto_mkdir`. Defaults to False.
             **kwargs: Keyword arguments that are passed to the `filesystem.open()` method.
 
         Return:
             (IO) File handler for the file at `self.path`.
         """
-        from squirrel.fsspec.fs import get_fs_from_url
-
-        fs = get_fs_from_url(self.path, storage_options={"auto_mkdir": create_if_not_exists})
-        return fs.open(self.path, mode=mode, **kwargs)
+        kwargs = {**self.storage_options, "auto_mkdir": create_if_not_exists, **kwargs}
+        return fsspec.open(self.path, mode=mode, **kwargs)
