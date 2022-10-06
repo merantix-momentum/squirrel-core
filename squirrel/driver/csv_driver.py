6d9cc3b43267d5b789e4449d6c2d2d2e28034670
Maximilian Schambach <maximilian.schambach@merantix.com>
2022-10-06 19:11:32 +0200

    Add driver option to specify csv read args
    - Allows for more flexible usage of CsvDriver in catalog

--- squirrel/driver/csv_driver.py
+++ squirrel/driver/csv_driver.py
@@ -15,7 +15,12 @@ class CsvDriver(FileDriver, DataFrameDriver):
 
     name = "csv"
 
-    def __init__(self, path: str, use_dask: bool = True, df_hooks: Iterable[Callable] | None = None, **kwargs) -> None:
+    def __init__(self,
+                 path: str,
+                 use_dask: bool = True,
+                 df_hooks: Iterable[Callable] | None = None,
+                 read_csv_kwargs: dict | None = None,
+                 **kwargs) -> None:
         """Initializes CsvDriver.
 
         Args:
@@ -23,30 +28,36 @@ class CsvDriver(FileDriver, DataFrameDriver):
             use_dask (bool): Whether to anychronously load the dataframe with dask.
             df_hooks (Iterable[Callable], optional): Preprocessing hooks to execute on the dataframe.
                 The first hook must accept a dask.dataframe.DataFrame or pandas.Dataframe in accordance with use_dask.
+            read_csv_kwargs: Arguments passed to all instances of :py:func:`dask.dataframe.read_csv` or
+                :py:func:`pandas.dataframe.read_csv` used in class methods.
             **kwargs: Keyword arguments passed to the super class initializer.
         """
         super().__init__(path, **kwargs)
         self.use_dask = use_dask
         self.df_hooks = df_hooks or []
+        self.read_csv_kwargs = read_csv_kwargs or {}
 
     def get_df(self, **kwargs) -> DataFrame | pd.DataFrame:
         """Returns the data in the .csv file as a Dask DataFrame.
 
         Args:
             **kwargs: Keyword arguments passed to :py:func:`dask.dataframe.read_csv` or
-                :py:func:`pandas.dataframe.read_csv` to read the .csv file.
+                :py:func:`pandas.dataframe.read_csv` to read the .csv file. Takes precedence over arguments specified at
+                class initialization.
 
         Returns:
             (dask.dataframe.DataFrame | pandas.DataFrame) Dask or Pandas DataFrame constructed from the .csv file.
         """
+        # Join kwargs for read_csv where kwargs takes precedence over self.read_csv_kwargs
+        read_csv_kwargs = {**self.read_csv_kwargs, **kwargs}
         if self.use_dask:
             import dask.dataframe as dd
 
-            result = dd.read_csv(self.path, **kwargs)
+            result = dd.read_csv(self.path, **read_csv_kwargs)
         else:
             import pandas as pd
 
-            result = pd.read_csv(self.path, **kwargs)
+            result = pd.read_csv(self.path, **read_csv_kwargs)
         for hook in self.df_hooks:
             result = hook(result)
 
@@ -61,13 +72,16 @@ class CsvDriver(FileDriver, DataFrameDriver):
             itertuples_kwargs: Keyword arguments to be passed to :py:meth:`dask.dataframe.DataFrame.itertuples`.
                 or :py:func:`pandas.dataframe.DataFrame.itertuples`
             read_csv_kwargs: Keyword arguments to be passed to :py:func:`dask.dataframe.read_csv` or
-                :py:func:`pandas.dataframe.read_csv`.
+                :py:func:`pandas.dataframe.read_csv`. Takes precedence over arguments specified at class initialization.
 
         Returns:
             (squirrel.iterstream.Composable) Iterable over the rows of the data frame as namedtuples.
         """
+        # Join kwargs for read_csv where read_csv_kwargs takes precedence over self.read_csv_kwargs
+        read_csv_kwargs = {**self.read_csv_kwargs, **read_csv_kwargs}
+
         if itertuples_kwargs is None:
-            itertuples_kwargs = dict()
+            itertuples_kwargs = {}
         if read_csv_kwargs is None:
-            read_csv_kwargs = dict()
+            read_csv_kwargs = {}
         return IterableSource(self.get_df(**read_csv_kwargs).itertuples(**itertuples_kwargs))
