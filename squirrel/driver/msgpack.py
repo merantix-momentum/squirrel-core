ce5e3a4e2e7baef0ddd4882f8f9b78c38d449794
Maximilian Schambach <maximilian.schambach@merantix.com>
2022-11-08 20:19:38 +0100

    Revert changes to msgpack driver

--- squirrel/driver/msgpack.py
+++ squirrel/driver/msgpack.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from typing import Any
+
 from squirrel.driver.store import StoreDriver
 from squirrel.serialization import MessagepackSerializer
 
@@ -11,8 +15,14 @@ class MessagepackDriver(StoreDriver):
 
     name = "messagepack"
 
-    def __init__(self, url: str, **kwargs):
-        """Initializes MessagepackDriver with default serializer."""
+    def __init__(self, url: str, storage_options: dict[str, Any] | None = None, **kwargs):
+        """Initializes MessagepackDriver with default serializer.
+
+        Args:
+            url (str): Path to the root directory. If this path does not exist, it will be created.
+            storage_options (Dict): a dictionary containing storage_options to be passed to fsspec.
+            **kwargs: Keyword arguments passed to the super class initializer.
+        """
         if "store" in kwargs:
             raise ValueError("Store of MessagepackDriver is fixed, `store` cannot be provided.")
-        super().__init__(url=url, serializer=MessagepackSerializer(), **kwargs)
+        super().__init__(url=url, serializer=MessagepackSerializer(), storage_options=storage_options, **kwargs)
